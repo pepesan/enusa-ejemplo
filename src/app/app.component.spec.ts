7027692232f0cc041294f0ba8fc2d043988e2808
pepesan <pepesan@gmail.com>
2024-07-02 09:42:44 +0200

    limpiado código de componente principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, enusa-ejemplo');
+    expect(compiled.querySelector('main')?.className).toContain('main');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Bienvenidas');
+    expect(compiled.querySelector('h3')?.textContent).toContain('enusa-ejemplo');
   });
 });
